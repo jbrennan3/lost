eb7c064299e219b725674628c7aa55bfed1bcf02
Jeremy Brennan <jeremb@gmail.com>
2017-01-13 02:16:46 +0000

    cleaned up bash script per user requirements

--- install_daemons.sh
+++ install_daemons.sh
@@ -4,31 +4,36 @@ function pause(){
     read -p "$*"
 }
 
+PATH=$1
+if [ $# -eq 0 ]
+    then PATH=$HOME
+fi
+
+#pause 'Install path to ' $PATH '? Press [Enter] to continue or Ctrl + C to exit.'
+
 git clone https://github.com/postgres/postgres.git
-cd postgres
 #pause 'Postgres downloaded press [Enter] to update to 9.5'
+cd postgres
 git checkout -b REL9_5_STABLE origin/REL9_5_STABLE
 git pull
-./configure --prefix=$1/installed
+./configure --prefix=$PATH/installed
+
 #pause 'Postgres updated press [Enter] to install build.'
 make
 make install
 cd ../
-echo Postgres finished.
+
 #pause 'Press [Enter] to continue with Apache setup.'
 curl http://www-us.apache.org/dist//httpd/httpd-2.4.25.tar.bz2 > httpd-2.4.25.tar.bz2
 tar -xjf httpd-2.4.25.tar.bz2
-cd httpd-2.4.25
+
 #pause 'Apache downloaded press [Enter] to continue with install build.'
-./configure --prefix=$1/installed
+cd httpd-2.4.25
+./configure --prefix=$PATH/installed
 make
 make install
 cd ../
-#cd $1/installed/bin
-#mkdir data
-#./initdb -D data
-#cd -
+
 echo ~~~~~~~~~~~~~~~~~~ Install Complete ~~~~~~~~~~~~~~~~~~~~~~~~~
 echo Make sure to change Apache Server listen port to desired port.
-echo Apache and Postgres installed to $1/installed.
-echo Postgres Database located at $1/installed/bin/data
+echo Apache and Postgres installed to $PATH/installed.
